80e197edf0c6e59bf707169da41645337088a244
Ahmed Dallel <ahmeddallel9@gmail.com>
2022-11-11 19:16:40 +0100

    hello

---
+++ app/src/main/java/com/example/tp_11/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tp_11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "debug");
+       /* Log.i("MainActivity", "info");
+        Log.w("MainActivity", "warning");*/
+        Log.e("MainActivity", "error");
+
+    }
+}
